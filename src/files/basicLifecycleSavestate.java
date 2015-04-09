6a20731bd4eb30a9b255b3613e86b7f8de94f8ba
bear <bear@prosoftsystems.ru>
2015-04-09 11:22:53 +0500

    Добавлена полная информация по второй лекции. Изменена структура документа.
    Signed-off-by: bear <bear@prosoftsystems.ru>

---
+++ src/files/basicLifecycleSavestate.java
@@ -0,0 +1,25 @@
+﻿public class MyActivity extends Activity {
+	public static final String KEY_VISIBLE = "com.parallels.sample.key.VISIBLE";
+	private View mView;
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putBoolean(mView.getVisibility() == VISIBLE);
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(outState);
+		mView.setVisibility(
+			savedInstanceState.getBoolean(KEY_VISIBLE) ? VISIBLE : GONE);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		if (savedInstanceState != null) {
+			mView.setVisibility(
+				savedInstanceState.getBoolean(KEY_VISIBLE) ? VISIBLE : GONE);
+		}
+	}
+}
---
+++ src/files/onConfigurationChanged.java
@@ -0,0 +1,10 @@
+﻿public void onConfiguratuibChanged(Configuration newConfig) {
+	super.onConfigurationChanged(newConfig);
+
+	// Checks the orientation on the screen
+	if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+		Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+	} else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+		Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+	}
+}
